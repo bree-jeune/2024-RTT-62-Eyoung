06efcafa40cd7b27312448d40de1281827520941
Bree Young <bree.j.32@gmail.com>
2024-05-03 05:07:02 -0700

    guided lab - GLAB_303_2_1

---
+++ src/com/perscholas/java_basics/ScannerExample.java
@@ -0,0 +1,10 @@
+package com.perscholas.java_basics;
+
+import java.util.Scanner;
+
+public class ScannerExample {
+    public static void main(String[] args) {
+        
+        Scanner sc1 = new Scanner(System.in);
+    }
+}
