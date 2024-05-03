0d54a145c8cb5ab1a5ecb653c53dd6d5935e493d
Bree Young <bree.j.32@gmail.com>
2024-05-03 06:02:21 -0700

    practice assignment - PA_303_2_1

--- src/PA_303__2_1/Homework303_2_1.java
+++ src/PA_303__2_1/Homework303_2_1.java
@@ -1,3 +1,5 @@
+package PA_303__2_1;
+
 public class Homework303_2_1 {
     public static void main(String[] args) {
 //        Write a program that declares two integer variables, assigns an integer to each, and adds them together. Assign the sum to a variable. Print out the result.
