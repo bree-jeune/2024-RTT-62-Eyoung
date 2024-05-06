eb914ca7e2df3bd3cb1ac6f47460c57146965b30
Bree Young <bree.j.32@gmail.com>
2024-05-05 23:26:07 -0700

    guided lab 303.5.2

---
+++ src/GLAB_303_5_2/SentinelValueDemo.java
@@ -0,0 +1,25 @@
+package GLAB_303_5_2;
+
+import java.util.Scanner;
+
+public class SentinelValueDemo {
+    public static void main(String[] args) {
+        // Create a Scanner
+        Scanner input = new Scanner(System.in);
+        // Read an initial data
+        System.out.print("Enter an integer value (the program exits if the input is 0): ");
+        int data = input.nextInt();
+        // Keep reading data until the input is 0
+        int sum = 0;
+        while (data != 0) {
+            sum += data;
+            // Read the next data
+            System.out.print("Enter an integer value (the program exits if the input is 0): ");
+            data = input.nextInt();
+        }
+        System.out.println("The sum is " + sum);
+
+        // Close the scanner when done
+        input.close();
+    }
+}
