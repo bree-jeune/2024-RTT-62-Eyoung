6137d2c5493fbbeb635ccc2289124917940fd449
bree-jeune <bree.j.32@gmail.com>
2024-07-29 09:09:53 -0700

    handling merge conflicts

---
+++ src/FirstJavaClass.java
@@ -0,0 +1,33 @@
+public class FirstJavaClass {
+    public static void main(String[] args) {
+        byte num;
+        num = 113;
+        System.out.println(num);
+
+        short snum;
+        snum = 150;
+        System.out.println(snum);
+
+        long lnum = -12332252626L;
+        System.out.println(lnum);
+
+        int integer = 122345;
+
+        double dnum = -42937737.9d;
+        System.out.println(dnum);
+
+        float fnum = 19.98f;
+        System.out.println(fnum);
+
+        boolean b = false;
+        System.out.println(b);
+
+        char ch = 'Z';
+        System.out.println(ch);
+
+        String str = "Hello World";
+
+
+
+    }
+}
---
+++ src/Homework303_2_1.java
@@ -0,0 +1,14 @@
+public class Homework303_2_1 {
+    public static void main(String[] args) {
+        //Write a program that declares two integer variables, assigns an integer to each, and adds them together
+
+        //Assign an integer to each
+
+        int num1 = 10;
+        int num2 = 20;
+
+        int sum = num1 + num2;
+
+        System.out.println(sum);
+    }
+}
